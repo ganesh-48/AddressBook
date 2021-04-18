3f0851de735f7f3cc1f316051a86549078bb0188
ganesh-48 <ganeshsrananavare@gmail.com>
2021-04-17 21:35:24 -0700

    ADDED:  Created a contact in address book system

--- src/main/java/com/addressbooksystem/AddressBookSystem.java
+++ src/main/java/com/addressbooksystem/AddressBookSystem.java
@@ -1,7 +1,14 @@
 package com.addressbooksystem;
 
+/*@Description-Welcome to address book system.
+*Create a contact in address book system.
+*Add contact details FirstName, lastname,address,city,state,zip,phone number and email. */
 public class AddressBookSystem {
     public static void main(String[] args) {
         System.out.println("Welcome to address book system");
+
+        //create a contact in address book 
+        Contacts contact = new Contacts("Ram", "Sharma", "Line No-3", "solapur", "maharashtra", 413101, 8767493484L, "abc@gmail.com");
+        System.out.println(contact.toString());
     }
 }
